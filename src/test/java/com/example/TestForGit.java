796605adb2f1a98a5d6975c2495d9e7c44ab07ef
175272511 <175272510@qq.com>
2016-03-19 21:15:19 +0800

    test

---
+++ src/test/java/com/example/TestForGit.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Created by leo on 2016/3/19.
+ */
+public class TestForGit {
+
+    public static void main(String[] args) {
+        System.out.printf("1");
+        System.out.printf("2");
+        System.out.printf("3");
+        System.out.printf("4");
+        System.out.printf("5");
+        System.out.printf("6");
+        System.out.printf("7");
+    }
+}
