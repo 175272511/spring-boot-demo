50a9bda584e4a1df67c502d1f2e61c5de7b0dba1
liuhui3 <liuhui3@meilele.com>
2016-12-05 11:20:03 +0800

    zookeeper

---
+++ src/main/java/com/example/zookeeper/EnsurePathDemo.java
@@ -0,0 +1,27 @@
+package com.example.zookeeper;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.ProtectACLCreateModePathAndBytesable;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.EnsurePath;
+
+public class EnsurePathDemo {
+
+    static String path = "/zk-book/c1";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("192.168.1.205:2181")
+            .sessionTimeoutMs(50000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+	public static void main(String[] args) throws Exception {
+		
+		client.start();
+		client.usingNamespace( "zk-book" );
+		EnsurePath ensurePath = new EnsurePath(path);
+		ensurePath.ensure(client.getZookeeperClient());
+		ensurePath.ensure(client.getZookeeperClient());   
+		
+		EnsurePath ensurePath2 = client.newNamespaceAwareEnsurePath("/c1");
+		ensurePath2.ensure(client.getZookeeperClient());
+	}
+}
