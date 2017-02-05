8c4d2c00b90d9c231b9e44b4fee9b5862df367bb
liuhui3 <liuhui3@meilele.com>
2017-02-05 11:41:13 +0800

    优化shiro

--- src/main/java/com/example/shiro/ChainDefinitionSectionMetaSource.java
+++ src/main/java/com/example/shiro/ChainDefinitionSectionMetaSource.java
@@ -1,7 +1,7 @@
-//package com.example.shiro;
+//package com.jlings.emall.shiro;
 //
-//import com.mll.sys.rbac.remote.service.AccountService;
-//import com.mll.sys.rbac.remote.vo.ResourceVo;
+//import com.jlings.mob.remote.service.AccountService;
+//import com.jlings.mob.remote.vo.RbacPermissionVo;
 //import org.apache.commons.lang3.StringUtils;
 //import org.apache.shiro.config.Ini;
 //import org.apache.shiro.config.Ini.Section;
@@ -20,72 +20,57 @@
 //public class ChainDefinitionSectionMetaSource implements
 //        FactoryBean<Section> {
 //
-//	private AccountService accountService;
+//    private AccountService accountService;
 //
-//	private ShiroSettings shiroSettings;
-//
-//	private List<ResourceVo> resourceList;
-//
-//	/**
-//	 * 获取应用的所有未删除的资源列表、可能包含禁用的资源
-//	 * @return
-//	 */
-//	public List<ResourceVo> getResourceList() {
-//		return resourceList;
-//	}
-//
-//	public void setAccountService(AccountService accountService) {
+//    public void setAccountService(AccountService accountService) {
 //        this.accountService = accountService;
 //    }
 //
-//	public void setShiroSettings(ShiroSettings shiroSettings) {
-//		this.shiroSettings = shiroSettings;
-//	}
 //
-//	// shiro默认的链接定义
-//	private String filterChainDefinitions;
+//    // shiro默认的链接定义
+//    private String filterChainDefinitions;
 //
-//	/**
-//	 * 通过filterChainDefinitions对默认的链接过滤定义
-//	 *
-//	 * @param filterChainDefinitions
-//	 *            默认的接过滤定义
-//	 */
-//	public void setFilterChainDefinitions(String filterChainDefinitions) {
-//		this.filterChainDefinitions = filterChainDefinitions;
-//	}
+//    /**
+//     * 通过filterChainDefinitions对默认的链接过滤定义
+//     *
+//     * @param filterChainDefinitions
+//     *            默认的接过滤定义
+//     */
+//    public void setFilterChainDefinitions(String filterChainDefinitions) {
+//        this.filterChainDefinitions = filterChainDefinitions;
+//    }
 //
-//	@Override
-//	public Section getObject() throws BeansException {
-//		Ini ini = new Ini();
-//		// 加载默认的url
-//		ini.load(filterChainDefinitions);
-//		Section section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
-//		// 获取appCode对应的所有的资源列表
-//		resourceList = accountService.searchResourceVoListBySysCode(shiroSettings.getAppCode());
-//		if(resourceList==null) resourceList = new ArrayList<ResourceVo>();
+//    @Override
+//    public Section getObject() throws BeansException {
+//        Ini ini = new Ini();
+//        // 加载默认的url
+//        ini.load(filterChainDefinitions);
+//        Section section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
+//        // 获取appCode对应的所有的资源列表
+//        List<RbacPermissionVo> permissionVos = accountService.findPermissions();
+//        if(permissionVos==null) permissionVos = new ArrayList<RbacPermissionVo>();
 //
-//		for (ResourceVo resource : resourceList) {
-//			if (StringUtils.isNotEmpty(resource.getResourceMatchUrl().trim()) &&
-//					StringUtils.isNotEmpty(resource.getResourceMatchPerm().trim())) {
-//				section.put(resource.getResourceMatchUrl(), "perms["+resource.getResourceMatchPerm()+"]");
-//			}
-//		}
+//        for (RbacPermissionVo resource : permissionVos) {
+//            if (StringUtils.isNotEmpty(resource.getPermissionUrl().trim()) &&
+//                    StringUtils.isNotEmpty(resource.getPermissionSign().trim())) {
+//                section.put(resource.getPermissionUrl(), "perms["+resource.getPermissionSign()+"]");
+//            }
+//        }
 //
-//		// 重置 /** 对应的 权限声明
-//		section.put("/**",section.remove("/**"));
+//        // 重置 /** 对应的 权限声明
+//        section.put("/**",section.remove("/**"));
 //
-//		return section;
-//	}
+//        return section;
+//    }
 //
-//	@Override
-//	public Class<?> getObjectType() {
-//		return Section.class;
-//	}
+//    @Override
+//    public Class<?> getObjectType() {
+//        return Section.class;
+//    }
 //
-//	@Override
-//	public boolean isSingleton() {
-//		return true;
-//	}
+//    @Override
+//    public boolean isSingleton() {
+//        return true;
+//    }
 //
 //}
