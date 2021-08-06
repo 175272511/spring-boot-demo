a8f86c4cc7e831e07466f3356ed00aac8542b4d6
liuhui <liuhui28@shein.com>
2021-08-06 14:02:35 +0800

    commit9

--- src/test/java/com/example/TestForGit.java
+++ src/test/java/com/example/TestForGit.java
@@ -7,7 +7,6 @@ public class TestForGit {
 
     public static void main(String[] args) {
         System.out.printf("1");
-        System.out.printf("2");
         System.out.printf("bugfix");
     }
 
