339e9124e80a681e16f4a2b290fa98a2e138f104
liuhui <liuhui28@shein.com>
2021-08-10 16:38:47 +0800

    commit15

--- src/test/java/com/example/TestForGit.java
+++ src/test/java/com/example/TestForGit.java
@@ -1,11 +1,21 @@
 package com.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+
+import java.io.IOException;
+
 /**
  * Created by leo on 2016/3/19.
  */
 public class TestForGit {
+    static OkHttpClient client = new OkHttpClient();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
     }
 
     public static void test(String[] args) {
