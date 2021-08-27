027a50ab012f43d6e33a671219c1b2f0ae80d28d
liuhui <liuhui28@shein.com>
2021-08-27 16:03:32 +0800

    1

--- src/test/java/com/example/TestForGit.java
+++ src/test/java/com/example/TestForGit.java
@@ -8,6 +8,7 @@ public class TestForGit {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
     }
 
     public static void test(String[] args) {
